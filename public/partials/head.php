577ff368dd779bee9350600c644a06ea69582c45
Lessleya <cru19010@byui.edu>
2025-10-08 17:27:08 -0600

    Update navbar and header styles

--- public/partials/head.php
+++ public/partials/head.php
@@ -11,6 +11,11 @@
 
     <!-- TMTrails CSS -->
     <link href="/assets/css/tmtrails.css?v=1552253809" rel="stylesheet">
+    <!-- Bootstrap JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
 
     <title>Traverse Moutain Trails Association</title>
     <meta name="description" content="Traverse Mountain Trails Association is a 501(c)3 nonprofit working in partnership with Lehi City to master-plan and fund a 60-mile network of trails on Traverse Mountain. Development of the trail system will require roughly a $2 million investment, which will come from public and private funds and grants. The network will connect to the Corner Canyon trail system in Draper and the Bonneville Shoreline Trail (BST). The Sensei Trail is the organization’s first open trail.">
--- public/partials/header.php
+++ public/partials/header.php
@@ -1,24 +1,24 @@
 <header id="tmtrails-header" class="mb-4">
-    <nav class="navbar navbar-expand-sm navbar-light">
+    <nav class="navbar navbar-expand-sm navbar-dark custom-header-bg custom-header-padding">
         <a class="navbar-brand" href="/">
-            <img class="tmtrails-logo" src="/assets/images/tmtrails-logo.svg" alt="tmtrails logo">
+            <img class="tmtrails-logo" src="/assets/images/tmtrails-logo-up.svg" alt="tmtrails logo">
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="/trails">Our Trails</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/donate">Donate</a>
-                </li>
-                <li class="nav-item">
+            <ul class="navbar-nav ml-auto ">
+                 <li class="nav-item">
                     <a class="nav-link" href="/about">About Us</a>
                 </li>
                 <li class="nav-item">
+                    <a class="nav-link" href="/trails">Our Trails</a>
+                </li>
+                  <li class="nav-item">
                     <a class="nav-link" href="/contact-us">Contact Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link btn btn-outline-light ml-2" href="/donate">Donate</a>
                 </li>
             </ul>
         </div>
