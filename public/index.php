fa4ba30474afa601f7a22348aaa6b5ef661ebafd
Lessleya <cru19010@byui.edu>
2025-11-03 08:43:25 -0700

    added timeline with messages

--- public/index.php
+++ public/index.php
@@ -67,6 +67,180 @@
                <div class="trails-container container bg-light rounded shadow p-5 my-5">
                 </div>
 
+
+
+<section class="timeline-section container-fluid  text-white py-5 position-relative">
+  <div class="container">
+    <div class="timeline-wrapper position-relative">
+      <!-- Horizontal scroll area -->
+      <div class="timeline-cards-wrapper">
+        <div class="d-flex flex-row flex-nowrap timeline-cards">
+
+          <!-- YEAR GROUP: 2019 -->
+          <div class="timeline-year-group d-flex flex-row position-relative mr-4">
+            <div class="timeline-year display-4 font-weight-light">2019</div>
+            <div class="d-flex flex-row">
+              <article class="timeline-card card bg-transparent border-0 text-white mr-3">
+                <img src="/assets/images/timeline/Sensei-Pic.png" class="card-img-top img-fluid" alt="Sensei trail">
+                <div class="card-body px-0">
+                  <h3 class="h3 font-weight-bold">Sensei</h3>
+                  <p class="card-subtitle mb-2">Lehi’s first multi-use natural surface trail officially opens.</p>
+                  <p class="card-desc mb-2">Funded by Lehi City, Adobe, and Recreational Trails Program Grant. Constructed by Wasatch Trails.</p>
+                  <p class="card-distance font-weight-bold">(5.08 mi)</p>
+                </div>
+              </article>
+
+              <article class="timeline-card card bg-transparent border-0 text-white mr-3">
+                <img src="/assets/images/timeline/Broadband.png" class="card-img-top img-fluid" alt="Broadband trail">
+                <div class="card-body px-0">
+                  <h3 class="h3 font-weight-bold">Broadband</h3>
+                  <p class="card-subtitle mb-2">Connecting the new Toll Brothers developments to Sensei.</p>
+                  <p class="card-desc mb-2">Funded by Toll Brothers. Constructed by Trail Ace.</p>
+                  <p class="card-distance font-weight-bold">(1.49 mi)</p>
+                </div>
+              </article>
+
+              <article class="timeline-card card bg-transparent border-0 text-white mr-3">
+                <img src="/assets/images/timeline/DialUpPic.png" class="card-img-top img-fluid" alt="Dial Up trail">
+                <div class="card-body px-0">
+                  <h3 class="h3 font-weight-bold">Dial Up</h3>
+                  <p class="card-subtitle mb-2">Extension from Sensei to loop from Fox Canyon.</p>
+                  <p class="card-desc mb-2">Funded by Lehi City and Recreational Trails Program. Constructed by Blazing Trailz.</p>
+                  <p class="card-distance font-weight-bold">(.46 mi)</p>
+                </div>
+              </article>
+            </div>
+          </div>
+
+          <!-- YEAR GROUP: 2020 -->
+          <div class="timeline-year-group d-flex flex-row position-relative mr-4">
+            <div class="timeline-year display-4 font-weight-light">2020</div>
+            <div class="d-flex flex-row">
+              <article class="timeline-card card bg-transparent border-0 text-white mr-3">
+                <img src="/assets/images/timeline/DialUpPic.png" class="card-img-top img-fluid" alt="Lolo trail">
+                <div class="card-body px-0">
+                  <h3 class="h3 font-weight-bold">Lolo</h3>
+                  <p class="card-subtitle mb-2">Additional trail funded by local grant and community donations.</p>
+                  <p class="card-desc mb-2">Funded by local grants and Traverse Mountain Trails Association. Managed by community partners and constructed by volunteers/contractors.</p>
+                  <p class="card-distance font-weight-bold">(2.65 mi)</p>
+                </div>
+              </article>
+
+              <article class="timeline-card card bg-transparent border-0 text-white mr-3">
+                <img src="/assets/images/timeline/Momentum.png" class="card-img-top img-fluid" alt="Momentum trail">
+                <div class="card-body px-0">
+                  <h3 class="h3 font-weight-bold">Momentum</h3>
+                  <p class="card-subtitle mb-2">Additional loop on the west leg of Sensei.</p>
+                  <p class="card-desc mb-2">Funded by Lehi City, Utah Outdoor Recreation Grant, and Traverse Mountain Trails Association. Constructed by Avid Trails.</p>
+                  <p class="card-distance font-weight-bold">(2.42 mi)</p>
+                </div>
+              </article>
+
+              <article class="timeline-card card bg-transparent border-0 text-white mr-3">
+                <img src="/assets/images/timeline/Traverse.png" class="card-img-top img-fluid" alt="Traverse trail">
+                <div class="card-body px-0">
+                  <h3 class="h3 font-weight-bold">Tráverse Trávérse</h3>
+                  <p class="card-subtitle mb-2">Connection to Draper City's Corner Canyon.</p>
+                  <p class="card-desc mb-2">Funded by Lehi City, Utah Outdoor Recreation Grant, and Traverse Mountain Trails Association. Constructed by Avid Trails.</p>
+                  <p class="card-distance font-weight-bold">(2.65 mi)</p>
+                </div>
+              </article>
+
+              <article class="timeline-card card bg-transparent border-0 text-white mr-3">
+                <img src="/assets/images/timeline/Footloose.png" class="card-img-top img-fluid" alt="Footloose trail">
+                <div class="card-body px-0">
+                  <h3 class="h3 font-weight-bold">Footloose</h3>
+                  <p class="card-subtitle mb-2">Lehi’s first hiking-only trail.</p>
+                  <p class="card-desc mb-2">Funded by Lehi City, Traverse Mountain Trails Association, and Utah Outdoor Recreation Grant. Constructed by Avid Trails.</p>
+                  <p class="card-distance font-weight-bold">(2.41 mi)</p>
+                </div>
+              </article>
+
+            </div>
+          </div>
+
+               <!-- YEAR GROUP: 2022 -->
+          <div class="timeline-year-group d-flex flex-row position-relative mr-4">
+            <div class="timeline-year display-4 font-weight-light">2022</div>
+            <div class="d-flex flex-row">
+              <article class="timeline-card card bg-transparent border-0 text-white mr-3">
+                <img src="/assets/images/timeline/BST.png" class="card-img-top img-fluid" alt="BST trail">
+                <div class="card-body px-0">
+                  <h3 class="h3 font-weight-bold">BST</h3>
+                  <p class="card-subtitle mb-2">Connector to Draper’s Brookside Trailhead.</p>
+                  <p class="card-desc mb-2">Funded by Lehi City, Traverse Mountain Trails Association, and Utah Outdoor Recreation Grant. Constructed by Blazing Trailz and Trail Ace.</p>
+                  <p class="card-distance font-weight-bold">(1.86 mi)</p>
+                </div>
+              </article>
+
+              <article class="timeline-card card bg-transparent border-0 text-white mr-3">
+                <img src="/assets/images/timeline/Sensei-Shortcut.png" class="card-img-top img-fluid" alt="Sensei Shortcut">
+                <div class="card-body px-0">
+                  <h3 class="h3 font-weight-bold">Sensei Shortcut</h3>
+                  <p class="card-subtitle mb-2">Bypass created during a city engineering project.</p>
+                  <p class="card-desc mb-2">Funded by Lehi City Engineering. Constructed by Blazing Trailz.</p>
+                  <p class="card-distance font-weight-bold">(.37 mi)</p>
+                </div>
+              </article>
+
+            </div>
+          </div>
+
+                <!-- YEAR GROUP: 2024 -->
+          <div class="timeline-year-group d-flex flex-row position-relative mr-4">
+            <div class="timeline-year display-4 font-weight-light">2024</div>
+            <div class="d-flex flex-row">
+              <article class="timeline-card card bg-transparent border-0 text-white mr-3">
+                <img src="/assets/images/timeline/Mayors.png" class="card-img-top img-fluid" alt="Mayors trail">
+                <div class="card-body px-0">
+                  <h3 class="h3 font-weight-bold">Mayors’</h3>
+                  <p class="card-subtitle mb-2">Connecting Lolo to Tráverse Trávérse.</p>
+                  <p class="card-desc mb-2">Made possible thanks to Mayor Johnson (Lehi) and Mayor Walker (Draper). Funded by Traverse Mountain Trails Association, Utah Outdoor Recreation Grant, and Lehi City. Constructed by Blazing Trailz.</p>
+                  <p class="card-distance font-weight-bold">(2.1 mi)</p>
+                </div>
+              </article>
+
+              <article class="timeline-card card bg-transparent border-0 text-white mr-3">
+                <img src="/assets/images/timeline/Lehis-Dream.png" class="card-img-top img-fluid" alt="Lehi's Dream">
+                <div class="card-body px-0">
+                  <h3 class="h3 font-weight-bold">Lehi’s Dream</h3>
+                  <p class="card-subtitle mb-2">Lehi’s first downhill mountain biking-only trail.</p>
+                  <p class="card-desc mb-2">Funded by Traverse Mountain Trails Association, Utah Outdoor Recreation Grant, and Lehi City. Constructed by Nature Trails.</p>
+                  <p class="card-distance font-weight-bold">(1.95 mi)</p>
+                </div>
+              </article>
+
+              <article class="timeline-card card bg-transparent border-0 text-white mr-3">
+                <img src="/assets/images/timeline/Lehis-Jump.png" class="card-img-top img-fluid" alt="Lehi's Jump Line">
+                <div class="card-body px-0">
+                  <h3 class="h3 font-weight-bold">Lehi’s Jump Line</h3>
+                  <p class="card-subtitle mb-2">Bike jump line connected to Lehi’s Dream.</p>
+                  <p class="card-desc mb-2">Funded by Traverse Mountain Trails Association, Utah Outdoor Recreation Grant, and Lehi City. Constructed by Nature Trails.</p>
+                  <p class="card-distance font-weight-bold">(.25 mi)</p>
+                </div>
+              </article>
+
+              <article class="timeline-card card bg-transparent border-0 text-white mr-3">
+                <img src="/assets/images/timeline/Stairway-To-Heaven.png" class="card-img-top img-fluid" alt="Stairway to Heaven">
+                <div class="card-body px-0">
+                  <h3 class="h3 font-weight-bold">Stairway to Heaven</h3>
+                  <p class="card-subtitle mb-2">A hiking-only trail connecting Fox Canyon to Deer Ridge.</p>
+                  <p class="card-desc mb-2">Funded by Traverse Mountain Trails Association, Utah Outdoor Recreation Grant, and Lehi City. Constructed by Blazing Trailz.</p>
+                  <p class="card-distance font-weight-bold">(1.75 mi)</p>
+                </div>
+              </article>
+            </div>
+          </div>
+
+          <!-- add more year groups here as needed -->
+
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
                 
                
 
